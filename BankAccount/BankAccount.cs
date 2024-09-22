da85a38b12bf6ff22a764a650ba6b4c69fa93d5c
benjamin burton <benjamin.alexander.burton@gmail.com>
2024-09-22 18:21:13 +0200

    klar med läxan

--- BankAccount/BankAccount.cs
+++ BankAccount/BankAccount.cs
@@ -8,12 +8,40 @@ namespace BankAccount
 {
     public class BankAccount
     {
-        //Lägg till Egenskaper (fields)
+        public string AccountNumber { get; set; }
+        public string AccountHolder { get; set; }
+        public decimal Balance { get; set; }
 
-        //Lägg till Konstruktor
+        public BankAccount(string accountNumber, string accountHolder, decimal balance)
+        {
+            AccountNumber = accountNumber;
+            AccountHolder = accountHolder;
+            Balance = balance;
+        }
 
-        //Lägg till Metoder
+        public void Deposit(decimal amount)
+        {
+            Balance = amount + Balance;
+            Console.WriteLine($"deposited: {amount}, new balance: {Balance}");
+        }
 
-        //Lycka till! :)
+        public void Withdraw(decimal amount)
+        {
+            Balance = Balance - amount;
+
+            if (amount > Balance)
+            {
+                Console.WriteLine("you don't have the sufficient amount in your account");
+            }
+            else
+            {
+                Console.WriteLine($"withdrew: {amount}, new balance: {Balance}");
+            }
+        }
+
+        public void DisplayBalance()
+        {
+            Console.WriteLine($"balance: {Balance}");
+        }
     }
 }
